3065c7b714e5823df67895f84b457d7d5f136562
tortas2719 <uzzielinu@gmail.com>
2023-07-07 18:29:45 -0600

    adding layout & some colors

--- resources/views/administrativeUnit/index.blade.php
+++ resources/views/administrativeUnit/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-800 leading-tight">
             {{ __('Unidades Administrativas') }}
         </h2>
     </x-slot>
@@ -13,6 +13,9 @@
             @endphp
         </div>
         @endif
+    </div>
+    <div class="flex flex-row-reverse">
+        <button onclick="location.href = '/administrativeUnit/create'" class="py-2 px-3 rounded-lg bg-green-500 text-white"><i class="fa-regular fa-plus"></i> Agregar Unidad</button>
     </div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test', function () {
+    return view('test');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
