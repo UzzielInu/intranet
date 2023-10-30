f57d79bcc7b4a922c38d886c9d903fc756862e5b
Tortas2719 <uzziel_56@hotmail.com>
2023-10-30 17:07:53 -0600

    header and sidebar

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -3,7 +3,7 @@
         <div class="flex items-center gap-2 sm:gap-4">
             <!-- Hamburger Toggle BTN -->
             <button
-                class="z-99999 block rounded-sm border border-stroke bg-red-600 p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark"
+                class="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark md:hidden"
                 @click.stop="sidebarToggle = !sidebarToggle" :value="deskToggle">
 
                 <span class="relative block h-5.5 w-5.5 cursor-pointer">
@@ -30,29 +30,11 @@
             </button>
             <!-- Hamburger Toggle BTN -->
             <button
-                class="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark"
+                class="hidden md:block z-99999 rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark"
                 @click.stop="deskToggle = !deskToggle" :value="deskToggle">
 
-                <span class="relative block h-5.5 w-5.5 cursor-pointer">
-                    <span class="du-block absolute right-0 h-full w-full">
-                        <span
-                            class="relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white"
-                            :class="{ '!w-full delay-300': !sidebarToggle }"></span>
-                        <span
-                            class="relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white"
-                            :class="{ '!w-full delay-400': !sidebarToggle }"></span>
-                        <span
-                            class="relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white"
-                            :class="{ '!w-full delay-500': !sidebarToggle }"></span>
-                    </span>
-                    <span class="du-block absolute right-0 h-full w-full rotate-45">
-                        <span
-                            class="absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white"
-                            :class="{ '!h-0 delay-[0]': !sidebarToggle }"></span>
-                        <span
-                            class="delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white"
-                            :class="{ '!h-0 dealy-200': !sidebarToggle }"></span>
-                    </span>
+                <span class="relative h-5.5 w-5.5 cursor-pointer">
+                    <i class="fa-solid fa-arrows-left-right-to-line"></i>
                 </span>
             </button>
         </div>
